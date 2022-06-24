e07d26286e8e93eb60c3f480881c94445b8afaaf
Leedabord <gwforumlakers@gmail.com>
2022-06-24 08:44:48 -0700

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -63,6 +63,22 @@ export class AppComponent implements OnInit {
     { "mark": 0, "w0": [0,0,0,0], "sum1": 0, "sum2": 0, "sum3": 0},
   ];
 
+  public tiles = [
+    { text: 'One', cols: 3, rows: 1, color: 'lightblue' },
+    { text: 'Two', cols: 1, rows: 2, color: 'lightgreen' },
+    { text: 'Three', cols: 1, rows: 1, color: 'lightpink' },
+    { text: 'Four', cols: 2, rows: 1, color: '#DDBDF1' },
+  ];
+
+  public color: string;
+
+  public availableColors = [
+    { name: 'none', color: '' },
+    { name: 'Primary', color: 'primary' },
+    { name: 'Accent', color: 'accent' },
+    { name: 'Warn', color: 'warn' }
+  ];
+
   constructor(
     private httpC: HttpClient, 
     private formB: FormBuilder
